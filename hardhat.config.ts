604a03d23be3a2cfb81cd15c5868d47167a825cc
Oliver Wahlen <oliver.wahlen@infinit-group.de>
2021-12-10 01:11:13 +0100

    setup enhancements
    - switched from ropsten to rinkeby for better chainlink support
    - bumped compiler version from 0.8.4 to 0.8.9
    - added launch.json to allow debugging in VSCode
    - gitignored dist folder

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -23,10 +23,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.4",
+  solidity: "0.8.9",
   networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
+    rinkeby: {
+      url: process.env.RINKEBY_URL || "",
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
